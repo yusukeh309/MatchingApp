00d320cdb760a581b8726f07478213f1ec8a2ea8
yusukeh309 <elmo.15vc@gmail.com>
2021-01-24 17:45:28 +0900

    first commit

---
+++ MatchingApp/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  MatchingApp
+//
+//  Created by Uske on 2021/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        let view1 = UIView()
+        view1.backgroundColor = .yellow
+        
+        let view2 = UIView()
+        view2.backgroundColor = .blue
+        
+        let view3 = UIView()
+        view3.backgroundColor = .green
+        
+        let stackView = UIStackView(arrangedSubviews: [view1, view2, view3])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        
+        self.view.addSubview(stackView)
+        
+        [
+            view1.heightAnchor.constraint(equalToConstant: 100),
+            view3.heightAnchor.constraint(equalToConstant: 100),
+            
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor)]
+            .forEach { $0.isActive = true }
+                
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
